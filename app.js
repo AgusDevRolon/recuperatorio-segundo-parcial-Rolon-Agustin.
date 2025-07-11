d0a040e03eca21a2224434fbbef5caab361e7ba8
Agustin Rolon <agustinxd364@gmail.com>
2025-07-11 10:16:51 -0300

    Realice las respectivas correcciones del codigo, importe el router correctamente por lo que las rutas (GET, POST, PUT y DELETE) ya responden, ahora app.listen si se ejecuta.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 
 import { sequelize, testConnection } from './src/config/database.js';
 import ProgrammingLanguage from './src/models/language.model.js';
+import router from './src/routes/language.routes.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API de Lenguajes de Programacion funcionando!...');
 });
+
+app.use('/api/languages', router );
 
 sequelize.sync({alter: true})
 .then(() => {
